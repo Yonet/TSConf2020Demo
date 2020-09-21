392412770bc8f2a87b43c028dd21693e7d7a663c
Aysegul Yonet <aysegulyonet@gmail.com>
2020-09-20 20:27:19 -0700

    Add xr helper

--- src/scenes/defaultWithTexture.ts
+++ src/scenes/defaultWithTexture.ts
@@ -1,19 +1,19 @@
-import { Engine } from "@babylonjs/core/Engines/engine";
-import { Scene } from "@babylonjs/core/scene";
-import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
-import { SphereBuilder } from "@babylonjs/core/Meshes/Builders/sphereBuilder";
-import { GroundBuilder } from "@babylonjs/core/Meshes/Builders/groundBuilder";
-import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
-import {CreateSceneClass} from "../createScene";
+import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera';
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
+import { Texture } from '@babylonjs/core/Materials/Textures/texture';
+import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+import { GroundBuilder } from '@babylonjs/core/Meshes/Builders/groundBuilder';
+import { SphereBuilder } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
+import { Scene } from '@babylonjs/core/scene';
+import { WebXRExperienceHelper } from '@babylonjs/core/XR/webXRExperienceHelper';
+
+import grassTextureUrl from '../../assets/grass.jpg';
+import { CreateSceneClass } from '../createScene';
 
 // If you don't need the standard material you will still need to import it since the scene requires it.
 // import "@babylonjs/core/Materials/standardMaterial";
-import { Texture } from "@babylonjs/core/Materials/Textures/texture";
-
-import grassTextureUrl from "../../assets/grass.jpg";
-
 export class DefaultSceneWithTexture implements CreateSceneClass {
 
     createScene = async (
@@ -22,7 +22,7 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
     ): Promise<Scene> => {
         // This creates a basic Babylon Scene object (non-mesh)
         const scene = new Scene(engine);
-    
+
         // This creates and positions a free camera (non-mesh)
         const camera = new ArcRotateCamera(
             "my first camera",
@@ -32,42 +32,43 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
             new Vector3(0, 0, 0),
             scene
         );
-    
+
         // This targets the camera to scene origin
         camera.setTarget(Vector3.Zero());
-    
+
         // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
-    
+
         // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
         const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    
+
         // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 0.7;
-    
+
         // Our built-in 'sphere' shape.
         const sphere = SphereBuilder.CreateSphere(
             "sphere",
             { diameter: 2, segments: 32 },
             scene
         );
-    
+
         // Move the sphere upward 1/2 its height
         sphere.position.y = 1;
-    
+
         // Our built-in 'ground' shape.
         const ground = GroundBuilder.CreateGround(
             "ground",
             { width: 6, height: 6 },
             scene
         );
-    
+
         // Load a texture to be used as the ground material
         const groundMaterial = new StandardMaterial("ground material", scene);
         groundMaterial.diffuseTexture = new Texture(grassTextureUrl, scene);
-    
+
         ground.material = groundMaterial;
-    
+        const xrHelper = await WebXRExperienceHelper.CreateAsync(scene);
+
         return scene;
     };
 }
