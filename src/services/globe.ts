70dbfe0953f66dcc8e858f37d9fdb65ece3ae0fe
Yonet <aysegulyonet@gmail.com>
2020-09-21 21:00:20 -0700

    before gts

---
+++ src/services/globe.ts
@@ -0,0 +1,19 @@
+// import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+// import { SphereBuilder } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
+// import { Scene } from '@babylonjs/core/scene';
+
+// export class globe {
+//     private sphere;
+//     private scene;
+//     constructor(scene: Scene) {
+//         this.scene = scene;
+//     };
+
+//     createGlobe(){   
+//         this.sphere = SphereBuilder.CreateSphere(
+//         "sphere",
+//         { diameter: 2, segments: 32 },
+//         this.scene
+//     );
+//     }
+// }
